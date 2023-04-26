c307133164eff53f8842c7bdc941446bdd9ee32f
lucasdefreitasroberto <68399974+lucasdefreitasroberto@users.noreply.github.com>
2023-04-26 16:50:26 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,26 +5,22 @@ app.use(express.json());
 
 const customers = [];
 
-// ## FinAPI - Financeira
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
 
-// ## Requisitos
-// - [X] Deve ser possível criar uma conta
-// - [X] Deve ser possível bsucar o extrato bancário do cliente
-// - [] Deve ser possível realizar um depósito
-// - [] Deve ser possível buscar o extrato bancário do cliente por data
-// - [] Deve ser possível atualizar dados da conta do cliente
-// - [] Deve ser possível deletar uma conta
+  const Customer = customers.find((customers) => customers.cpf === cpf);
 
-// ## Regras de negócio
-// - [X] Não Deve ser possível cadastrar uma conta com CPF já exitente
-// - [X] Não Deve ser possível fazer depósito em uma conta não existente
-// - [] Não Deve ser possível buscar extrato em uma conta não existente
-// - [] Não Deve ser possível fazer saque em uma conta não existente
-// - [] Não Deve ser possível excluir uma conta não existente
-// - [] Não Deve ser possível fazer saque quando o saldo por insuficiente
+  
+  if (!Customer) {
+    return response.status(400).json({ error: "Customer Not Found" });
+  }
 
-app.get("/", (req, res) => {
-  return res.json(["API Financeira"]);
+  return next;
+}
+
+app.get("/", (request, response) => {
+  return response.json(["API Financeira"]);
 });
 
 app.get("/statement", (request, response) => {
